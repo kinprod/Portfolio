9ca0adfe021097c969a1d64d24284a798b40aba8
K.I.N. <Speedcubingrussia@gmail.com>
2025-12-02 00:52:47 +0300

    fix url

--- headerClick.js
+++ headerClick.js
@@ -2,7 +2,7 @@ const mainUrl = document.getElementById("mainUrl");
 const github = document.getElementById("GitHub");
 
 mainUrl.onclick = () => {
-    window.location.href = "./Main site/index.html";
+    window.location.href = "/Main site/index.html";
 };
 
 github.onclick = () => {
